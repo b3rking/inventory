f3cd19026040663d222963ed9746dab516501b93
b3rking <ishimweeschylebersy@gmail.com>
2022-10-28 14:35:33 +0200

    add boilplate for the dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,33 +7,29 @@
     <link rel="stylesheet" href="/css/app.css" />
     <title>Inventory - @yield('title', 'dashboard')</title>
 </head>
-<body>
-    <header>
-    <h1><a href="{{ route('dashboard') }}" class="text-2xl">DASHBOARD</a></h1>
-    @auth    
-    <p>{{ Auth::user()->name }}</p>
-    <form action="/auth/logout" method="post">
-        @csrf
-        <button type="submit">logout</button>
-    </form>
-    </header>
-    @endauth
-    <main>
-        <div class="side-menu">
+<body class="bg-gray-50">
+    <header class="flex flex-row justify-between py-2 px-6">
+    <h1><a href="{{ route('dashboard') }}" class="text-2xl font-bold">DASHBOARD</a></h1>
+    <div class="flex flex-row justify-around">
+        @auth    
+        <p class="mx-2 font-semibold">{{ Auth::user()->name }}</p>
+        <form action="/auth/logout" method="post">
+            @csrf
+            <button type="submit">logout</button>
+        </form>
+        </header>
+        @endauth
+    </div>
+    <main class="px-4 py-2 flex flex-row">
+        <div class="side-menu w-1/6 bg-gray-200 rounded-md p-3 h-auto max-h-96">
             <ul>
-                <li><a href="{{ route('categories.index') }}">Categories</a></li>
-            </ul>
-            <ul>
-                <li><a href="{{ route('products.index') }}">Products</a></li>
-            </ul>
-            <ul>
-                <li><a href="{{ route('clients.index') }}">Clients</a></li>
-            </ul>
-            <ul>
-                <li><a href="{{ route('users.index') }}">Users</a></li>
+                <li class="py-2 px-4 font-semibold rounded-md hover:bg-slate-50"><a href="{{ route('categories.index') }}">Categories</a></li>
+                <li class="py-2 px-4 font-semibold rounded-md hover:bg-slate-50"><a href="{{ route('products.index') }}">Products</a></li>
+                <li class="py-2 px-4 font-semibold rounded-md hover:bg-slate-50"><a href="{{ route('clients.index') }}">Clients</a></li>
+                <li class="py-2 px-4 font-semibold rounded-md hover:bg-slate-50"><a href="{{ route('users.index') }}">Users</a></li>
             </ul>
         </div>
-        <div class="content">
+        <div class="content px-4 py-4 flex flex-row bg-gray-200 rounded-md p-3 ml-4 w-screen">
             @yield('content')
         </div>
     </main>
